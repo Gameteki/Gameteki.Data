ef28a288b74189e65f64e49f91a0d6bc1d68d714
Stuart Walsh <stu@ipng.org.uk>
2018-12-30 12:50:25 +0000

    Add message for multiple user disconnect

--- Constants/RedisChannels.cs
+++ Constants/RedisChannels.cs
@@ -4,6 +4,7 @@
     {
         public const string NewUser = "NewUser";
         public const string UserDisconnect = "UserDisconnect";
+        public const string UsersDisconnect = "UsersDisconnect";
         public const string LobbyHello = "LobbyHello";
         public const string LobbyHeartbeat = "LobbyHeartbeat";
     }
