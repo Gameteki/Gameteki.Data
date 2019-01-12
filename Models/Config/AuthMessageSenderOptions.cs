7916869d3f7498e2adadf69f4949d9ab07232f23
Stuart Walsh <stu@ipng.org.uk>
2019-01-12 20:11:24 +0000

    email settings belongs in the api only

--- Models/Config/AuthMessageSenderOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CrimsonDev.Gameteki.Data.Models.Config
-{
-    internal class AuthMessageSenderOptions
-    {
-        public string SendGridUser { get; set; }
-        public string SendGridKey { get; set; }
-    }
-}
