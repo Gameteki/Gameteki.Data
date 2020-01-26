e6c2dfb8baf678beed5f31216d20e557bd1879da
Stuart Walsh <stu@ipng.org.uk>
2020-01-26 22:49:22 +0000

    Add patreon options object

---
+++ Models/Config/PatreonOptions.cs
@@ -0,0 +1,8 @@
+namespace CrimsonDev.Gameteki.Data.Models.Config
+{
+    public class PatreonOptions
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
