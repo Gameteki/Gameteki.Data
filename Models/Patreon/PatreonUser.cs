9f9fc70f949b788eeeabb2056d4292b07dcd710d
Stuart Walsh <stu@ipng.org.uk>
2020-01-27 22:13:35 +0000

    update json properties

--- Models/Patreon/PatreonUser.cs
+++ Models/Patreon/PatreonUser.cs
@@ -1,7 +1,10 @@
 namespace CrimsonDev.Gameteki.Data.Models.Patreon
 {
+    using System.Text.Json.Serialization;
+
     public class PatreonUser
     {
+        [JsonPropertyName("attributes")]
         public PatreonUserAttributes Attributes { get; set; }
         public string Id { get; set; }
         public string Type { get; set; }
--- Models/Patreon/PatreonUserResponse.cs
+++ Models/Patreon/PatreonUserResponse.cs
@@ -1,9 +1,11 @@
 namespace CrimsonDev.Gameteki.Data.Models.Patreon
 {
     using System;
+    using System.Text.Json.Serialization;
 
     public class PatreonUserResponse
     {
+        [JsonPropertyName("data")]
         public PatreonUser Data { get; set; }
     }
 }
