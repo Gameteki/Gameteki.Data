eb208de29f85413264a72ee691822dba45318d66
Stuart Walsh <stu@ipng.org.uk>
2020-02-02 20:52:38 +0000

    Add logout request model

---
+++ Models/Api/Request/LogoutRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CrimsonDev.Gameteki.Data.Models.Api
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LogoutRequest
+    {
+        [Required]
+        public string RefreshToken { get; set; }
+    }
+}
