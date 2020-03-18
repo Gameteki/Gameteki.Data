be067db962fb1b212592667aad5d00706a6fe535
Stuart Walsh <stu@ipng.org.uk>
2020-03-18 19:37:13 +0000

    rename permissions

--- Models/Api/ApiUser.cs
+++ Models/Api/ApiUser.cs
@@ -8,7 +8,7 @@
         public string Username { get; set; }
         public string Email { get; set; }
         public ApiSettings Settings { get; set; }
-        public GametekiPermissions GametekiPermissions { get; set; }
+        public GametekiPermissions Permissions { get; set; }
         public string CustomData { get; set; }
         public PatreonStatus PatreonStatus { get; set; }
     }
